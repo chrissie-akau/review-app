1a998a04d887ebfe87fdc327b89190eafcd4e0c5
Chrissie Akau <6412502-chrissie-akau@users.noreply.gitlab.com>
2020-08-18 15:32:50 -0700

    react-router-dom, styled components yarn added, components folder with Airline show adn Airline index added

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Airline = () => {
+    return (  
+        <div>
+            <h1>This is the Airlines#show view for our app.</h1>
+        </div>
+    )
+}
+ 
+export default Airline
